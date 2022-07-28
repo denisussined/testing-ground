97cc42e87182163e5505dd0eb09a3145d0905459
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-07-28 11:30:44 +0300

    App; Index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import NavbarItem from "./components/navbar/NavbarItem";
+import Dropdown from "./components/dropdown/Dropdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    return (
+        <div className={"app"}>
+
+            <Header>
+                <h1>Header</h1>
+                <Navbar>
+                    <NavbarItem button={<span>1</span>}/>
+                    <NavbarItem button={<span>2</span>}/>
+                    <NavbarItem button={<span>3</span>}>
+                        <Dropdown />
+                    </NavbarItem>
+                </Navbar>
+            </Header>
+
+            <main className={"main"}>
+                main
+            </main>
+
+            <footer className={"footer"}>
+                footer
+            </footer>
+        </div>
+    )
 }
 
 export default App;
